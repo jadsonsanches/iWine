be27ad39fa02140f662e51c621da059658435ffd
jadson.sanches <jadson.sanches@7comm.com.br>
2022-12-08 15:47:11 -0300

    start

---
+++ src/main/java/com/api/iwine/IwineApplication.java
@@ -0,0 +1,13 @@
+package com.api.iwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/iwine/IwineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.iwine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
