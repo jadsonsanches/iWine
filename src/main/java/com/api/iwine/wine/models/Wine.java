4a48c5c7ed4b706af4ac28169d808066204ef30f
jadson.sanches <jadson.sanches@7comm.com.br>
2022-12-08 16:09:44 -0300

    Added @Entity

--- src/main/java/com/api/iwine/wine/models/Wine.java
+++ src/main/java/com/api/iwine/wine/models/Wine.java
@@ -5,10 +5,14 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "wine")
 public class Wine implements Serializable {
 
   private static final long serialVersionUID = 1L;
