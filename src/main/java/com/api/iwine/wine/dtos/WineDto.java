e671296e73f6f0a2cab14a84e827cad2cbf03c95
jadson.sanches <jadson.sanches@7comm.com.br>
2022-12-09 16:35:20 -0300

    Added wine dto

---
+++ src/main/java/com/api/iwine/wine/dtos/WineDto.java
@@ -0,0 +1,41 @@
+package com.api.iwine.wine.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class WineDto {
+
+  @NotBlank
+  @Size(max = 100)
+  private String name;
+
+  @NotBlank
+  @Size(max = 20)
+  private String type;
+
+  @NotBlank
+  @Size(max = 30)
+  private String grape;
+
+  @NotBlank
+  @Size(max = 30)
+  private String country;
+
+  private Float alcoholContent;
+
+  private Integer volume;
+
+  private Float rate;
+
+  @NotBlank
+  @Size(max = 500)
+  private String description;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+}
