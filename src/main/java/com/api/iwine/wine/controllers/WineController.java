bfcf848424f396a481063228bc2bff7e8d75879b
jadson.sanches <jadson.sanches@7comm.com.br>
2022-12-16 22:35:47 -0300

    Added getWine, getWineById, update, delete and save new wine.

--- src/main/java/com/api/iwine/wine/controllers/WineController.java
+++ src/main/java/com/api/iwine/wine/controllers/WineController.java
@@ -1,14 +1,30 @@
 package com.api.iwine.wine.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.iwine.wine.dtos.WineDto;
 import com.api.iwine.wine.models.Wine;
 import com.api.iwine.wine.services.WineService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -24,6 +40,54 @@ public class WineController {
   @GetMapping
   public List<Wine> findAll() {
     return wineService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
+    Optional<Wine> wine = wineService.findById(id);
+
+    if (!wine.isPresent())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Wine not found");
+
+    return ResponseEntity.status(HttpStatus.OK).body(wine.get());
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> create(@RequestBody @Valid WineDto wine) {
+    var newWine = new Wine();
+
+    BeanUtils.copyProperties(wine, newWine);
+    newWine.setCreatedAt(LocalDateTime.now(ZoneId.of("UTC")));
+    newWine.setUpdatedAt(LocalDateTime.now(ZoneId.of("UTC")));
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(wineService.save(newWine));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> update(@PathVariable(value = "id") UUID id, @RequestBody @Valid WineDto wineUpdate) {
+    Optional<Wine> wineFinded = wineService.findById(id);
+
+    if (!wineFinded.isPresent())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Wine not found");
+
+    Wine wine = new Wine();
+    BeanUtils.copyProperties(wine, wineUpdate);
+    wine.setId(wineFinded.get().getId());
+    wine.setUpdatedAt(wineFinded.get().getUpdatedAt());
+
+    return ResponseEntity.status(HttpStatus.OK).body(wineService.save(wine));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
+    Optional<Wine> wineFinded = wineService.findById(id);
+
+    if (!wineFinded.isPresent())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Wine not found");
+
+    wineService.delete(wineFinded.get());
+
+    return ResponseEntity.status(HttpStatus.OK).body("Wine deleted succesfully");
   }
 
 }
--- src/main/java/com/api/iwine/wine/services/WineService.java
+++ src/main/java/com/api/iwine/wine/services/WineService.java
@@ -1,11 +1,15 @@
 package com.api.iwine.wine.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.api.iwine.wine.models.Wine;
 import com.api.iwine.wine.repositories.WineRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class WineService {
@@ -20,4 +24,17 @@ public class WineService {
     return wineRepository.findAll();
   }
 
+  public Optional<Wine> findById(UUID id) {
+    return wineRepository.findById(id);
+  }
+
+  @Transactional
+  public Object save(Wine newWine) {
+    return wineRepository.save(newWine);
+  }
+
+  @Transactional
+  public void delete(Wine wine) {
+    wineRepository.delete(wine);
+  }
 }
